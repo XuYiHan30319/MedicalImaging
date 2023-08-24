af5b84d2840d75d7b0a4239a1e2c8bf461648e15
许一涵 <110003881+XuYiHan30319@users.noreply.github.com>
2023-08-24 14:44:56 +0800

    unet

--- unet/UNet.py
+++ unet/UNet.py
@@ -72,7 +72,7 @@ class UpBlock(nn.Module):
     def __init__(self, inChannel, outChannel) -> None:
         super().__init__()
         self.ct = nn.ConvTranspose2d(inChannel, outChannel, 2, stride=2)
-        
+
         self.net = nn.Sequential(
             nn.Conv2d(inChannel, outChannel, 3, padding=0),
             nn.BatchNorm2d(outChannel),
@@ -84,14 +84,15 @@ class UpBlock(nn.Module):
 
     def forward(self, x, pre):
         x = self.ct(x)
-        #裁剪
+        # 裁剪
         size = x.size()[2]
         preSize = pre.size()[2]
-        start_row  = (preSize-size)//2
+        start_row = (preSize - size) // 2
         end_row = start_row + size
-        cropped_tensor = pre[:,:,start_row:end_row, start_row:end_row]    
+        cropped_tensor = pre[:, :, start_row:end_row, start_row:end_row]
         x = torch.cat([x, cropped_tensor], dim=1)
         return self.net(x)
+
 
 class UNet(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
@@ -107,18 +108,18 @@ class UNet(nn.Module):
             nn.Conv2d(1024, 1024, 3, padding=0),
             nn.ReLU(),
         )
-        
+
         self.up1 = UpBlock(1024, 512)
         self.up2 = UpBlock(512, 256)
         self.up3 = UpBlock(256, 128)
         self.up4 = UpBlock(128, 64)
-        self.final = nn.Conv2d(in_channels=64,out_channels=1,kernel_size=1)
+        self.final = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1)
 
     def forward(self, x):
-        pre1,x = self.down1(x)
-        pre2,x = self.down2(x)
-        pre3,x = self.down3(x)
-        pre4,x = self.down4(x)
+        pre1, x = self.down1(x)
+        pre2, x = self.down2(x)
+        pre3, x = self.down3(x)
+        pre4, x = self.down4(x)
         x = self.center(x)
         print(x.size())
         x = self.up1(x, pre4)
@@ -129,19 +130,20 @@ class UNet(nn.Module):
         x = self.final(x)
         return x
 
-def train(net,data_loader,loss_fn,optimizer,num_epochs,device):
+
+def train(net, data_loader, loss_fn, optimizer, num_epochs, device):
     print("Start Training...")
     loss_fn = nn.BCEWithLogitsLoss()
     for epoch in range(num_epochs):
         net.train()  # 设置网络为训练模式
         total_loss = 0.0
         for batch_idx, (inputs, targets) in enumerate(data_loader):
-            inputs,targets = inputs.to(device),targets.to(device)
+            inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()  # 梯度清零
             # 前向传播
             outputs = net(inputs)
             # targets = targets[:,0,:,:]
-            # outputs = outputs[:, 0, :, :]            
+            # outputs = outputs[:, 0, :, :]
             # 计算损失
             loss = loss_fn(outputs, targets)
             # 反向传播
@@ -150,16 +152,25 @@ def train(net,data_loader,loss_fn,optimizer,num_epochs,device):
             optimizer.step()
             total_loss += loss.item()
             if (batch_idx + 1) % 10 == 0:
-                print(f"Epoch [{epoch+1}/{num_epochs}] Batch [{batch_idx+1}/{len(data_loader)}] Loss: {loss.item():.4f}")
+                print(
+                    f"Epoch [{epoch+1}/{num_epochs}] Batch [{batch_idx+1}/{len(data_loader)}] Loss: {loss.item():.4f}"
+                )
         average_loss = total_loss / len(data_loader)
         print(f"Epoch [{epoch+1}/{num_epochs}] Average Loss: {average_loss:.4f}")
 
-device = torch.device("cuda" if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available else "cpu")
+
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available
+    else "cpu"
+)
 device = torch.device("cpu")
 Data = Data()
 Data = DataLoader(Data, batch_size=2, shuffle=True)
 Net = UNet().to(device)
 optimizer = torch.optim.Adam(Net.parameters(), lr=0.001)
 loss_fn = nn.CrossEntropyLoss()
-train(Net,Data,loss_fn,optimizer,10,device)
-Net._save_to_state_dict('1.pth')
+train(Net, Data, loss_fn, optimizer, 100, device)
+Net._save_to_state_dict("1.pth")
