01d6ab3be038e3a7617197590ddf9235f8b9cb7c
许一涵 <110003881+XuYiHan30319@users.noreply.github.com>
2023-08-28 23:25:39 +0800

    TransUNet

--- AttentionNet/TransUnet.py
+++ AttentionNet/TransUnet.py
@@ -19,13 +19,55 @@ class UnetBlock(nn.Module):
         return x
 
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self):
-        pass
+class AttentionBlock(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_heads) -> None:
+        super().__init__()
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.q = nn.Linear(input_dim, input_dim)
+        self.k = nn.Linear(input_dim, input_dim)
+        self.v = nn.Linear(input_dim, input_dim)
+        self.msa = nn.MultiheadAttention(input_dim, num_heads)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim),
+        )
 
     def forward(self, x):
-        pass
+        x1 = self.norm1(x)
+        q = self.q(x1)
+        k = self.k(x1)
+        v = self.v(x1)
+        x1, _ = self.msa(q, k, v)
+        x1 = x1 + x
 
+        x2 = self.norm2(x1)
+        x2 = self.mlp(x2)
+        x2 = x1 + x2
+        return x2
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, image_size,patch_size,in_channel,embed_dim) -> None:
+        super().__init__()
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.grid_size = image_size // patch_size
+        self.num_patches = self.grid_size ** 2
+        
+        self.proj = nn.Conv2d(in_channel,embed_dim,kernel_size=patch_size,stride=patch_size)
+        # self.norm = nn.LayerNorm(embed_dim)
+        
+    def forward(self,x):
+        x = self.proj(x)
+        x = x.flatten(2).transpose(1,2)
+        # x = self.norm(x)
+        return x
 
 class TransUnet(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -40,6 +82,8 @@ class TransUnet(nn.Module):
         self.down3 = UnetBlock(ch[1], ch[2])
         self.down4 = UnetBlock(ch[2], ch[3])
 
+
+
         self.up1 = UnetBlock(512 * 2, 256)
         self.up2 = UnetBlock(256 * 2, 128)
         self.up3 = UnetBlock(128 * 2, 64)
@@ -47,10 +91,12 @@ class TransUnet(nn.Module):
         self.final = nn.Conv2d(16, out_channels, kernel_size=1)  # 最后的输出层
 
     def forward(self, x):
+        # 假设图片大小为512*512
         x1 = self.down1(x)  # 16*H*W
         x2 = self.down2(self.maxpool(x1))  # 64*H/2*W/2
         x3 = self.down3(self.maxpool(x2))  # 128*H/4*W/4
-        x4 = self.down4(self.maxpool(x3))  # 256*H/8*W/8
+        x4 = self.down4(self.maxpool(x3))  # 256*64*64
+        # 然后吧图片分为4*4个小块，每个小块为16*16*256
         """
             这里暂时跳过了多头注意力机制
             首先把x4的输出转变为多头注意力的输入格式:(H/8*W/8)*256
@@ -66,7 +112,6 @@ class TransUnet(nn.Module):
 
 
 if __name__ == "__main__":
-    print("hello worldr")
     device = torch.device(
         "cuda"
         if torch.cuda.is_available()
@@ -75,5 +120,3 @@ if __name__ == "__main__":
         else "cpu"
     )
     model = TransUnet(3, 1).to(device)
-    
-    
