49753d2456fc2d638b45be1e1cf056e3831106b9
许一涵 <110003881+XuYiHan30319@users.noreply.github.com>
2023-08-31 15:06:09 +0800

    66

--- AttentionNet/VIT.py
+++ AttentionNet/VIT.py
@@ -69,52 +69,6 @@ class Data(Dataset):
         return self.x.__len__()
 
 
-class UnetBlock(nn.Module):
-    def __init__(self, inChannel, outChannel) -> None:
-        # 使用卷积核大小3，步长1，填充1，保持大小不变，从而让神经网络绝对对称,可以吧下降和上升写在一起
-        # 在transunet中只有一层哦
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(inChannel, outChannel, 3, padding=1, stride=1),
-            nn.BatchNorm2d(outChannel),
-            nn.ReLU(),
-        )
-
-    def forward(self, x):
-        x = self.net(x)
-        return x
-
-
-class TransformerBlock(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_heads, drop=0.2) -> None:
-        super().__init__()
-        self.norm1 = nn.LayerNorm(input_dim)
-        self.q = nn.Linear(input_dim, input_dim)
-        self.k = nn.Linear(input_dim, input_dim)
-        self.v = nn.Linear(input_dim, input_dim)
-        self.msa = nn.MultiheadAttention(input_dim, num_heads)
-        self.norm2 = nn.LayerNorm(input_dim)
-        self.mlp = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(drop),  # 添加 Dropout 层
-            nn.Linear(hidden_dim, input_dim),
-        )
-
-    def forward(self, x):
-        x1 = self.norm1(x)
-        q = self.q(x1)
-        k = self.k(x1)
-        v = self.v(x1)
-        x1, _ = self.msa(q, k, v)
-        x1 = x1 + x
-
-        x2 = self.norm2(x1)
-        x2 = self.mlp(x2)
-        x2 = x1 + x2
-        return x2
-
-
 class PatchEmbedding(nn.Module):
     def __init__(self, image_size, patch_size, in_channel, embed_dim) -> None:
         # imagesize是图片大小,pathsize是每个patch的大小,embed_dim是每个patch的维度,维度的计算为inchanel*patchsize*patchsize
@@ -161,67 +115,61 @@ class PositionalEncoding(nn.Module):
         return x + pos_encoding
 
 
-class TransUnet(nn.Module):
-    def __init__(self, in_channels, out_channels, embed_dim=512):
-        super(TransUnet, self).__init__()
-        ch = [16, 64, 128, 256, 512]
-        # self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.softmax = nn.Softmax()
-        self.position = PositionalEncoding(embed_dim)
-        self.embed_dim = embed_dim
-
-        self.down1 = UnetBlock(in_channels, ch[0])
-        self.down2 = UnetBlock(ch[0], ch[1])
-        self.down3 = UnetBlock(ch[1], ch[2])
-        self.down4 = UnetBlock(ch[2], ch[3])
-        self.patchEmbedding = PatchEmbedding(64, 2, 256, embed_dim)
-        self.Transform = nn.Sequential(
-            *[TransformerBlock(embed_dim, embed_dim * 3, 8) for _ in range(12)]
+class ViTBlock(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_heads, drop=0.2) -> None:
+        super().__init__()
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.q = nn.Linear(input_dim, input_dim)
+        self.k = nn.Linear(input_dim, input_dim)
+        self.v = nn.Linear(input_dim, input_dim)
+        self.msa = nn.MultiheadAttention(input_dim, num_heads)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(drop),  # 添加 Dropout 层
+            nn.Linear(hidden_dim, input_dim),
         )
-        self.conv1 = nn.Conv2d(embed_dim, 512, kernel_size=3, stride=1, padding=1)
-
-        self.ct1 = nn.ConvTranspose2d(
-            512, 256, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.up1 = UnetBlock(512, 256)
-        self.ct2 = nn.ConvTranspose2d(
-            256, 128, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.up2 = UnetBlock(256, 128)
-        self.ct3 = nn.ConvTranspose2d(
-            128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.up3 = UnetBlock(128, 64)
-        self.ct4 = nn.ConvTranspose2d(
-            64, 16, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.up4 = UnetBlock(64, 16)
-        self.final = nn.Conv2d(16, out_channels, kernel_size=1)  # 最后的输出层
 
     def forward(self, x):
-        # 假设图片大小为512*512
-        batchsize, n, h, w = x.size()
-        x1 = self.down1(x)  # 16*H*W
-        x2 = self.down2(self.maxpool(x1))  # 64*H/2*W/2
-        x3 = self.down3(self.maxpool(x2))  # 128*H/4*W/4
-        x4 = self.down4(self.maxpool(x3))  # 256*64*64
-        x5 = self.patchEmbedding(x4)  # 得到序列化的结果,大小为16*(16*256*16)
-        x5 = self.position(x5)
-        out = self.Transform(x5)
-        out = (
-            torch.reshape(out, (batchsize, h // 16, w // 16, self.embed_dim))
-            .transpose(1, 3)
-            .transpose(2, 3)
+        x1 = self.norm1(x)
+        q = self.q(x1)
+        k = self.k(x1)
+        v = self.v(x1)
+        x1, _ = self.msa(q, k, v)
+        x1 = x1 + x
+
+        x2 = self.norm2(x1)
+        x2 = self.mlp(x2)
+        x2 = x1 + x2
+        return x2
+
+
+class VIT(nn.Module):
+    def __init__(
+        self, inchannel, out_channels, imageSize=512, embedDim=256, patchSize=16
+    ):
+        super(VIT, self).__init__()
+        self.embeding = PatchEmbedding(
+            image_size=imageSize,
+            patch_size=16,
+            in_channel=inchannel,
+            embed_dim=embedDim,
         )
-        out = self.conv1(out)
-        up1 = self.up1(torch.cat([x4, self.ct1(out)], dim=1))
-        up2 = self.up2(torch.cat([x3, self.ct2(up1)], dim=1))
-        up3 = self.up3(torch.cat([x2, self.ct3(up2)], dim=1))
-        up4 = self.ct4(up3)
-        out = self.final(up4)
-        return out
+        self.net = nn.Sequential(
+            *[ViTBlock(input_dim=256, hidden_dim=512, num_heads=8) for _ in range(12)]
+        )
+        self.position = PositionalEncoding(
+            embed_dim=embedDim,
+        )
+
+    def forward(self, x):
+        batch, n, w, h = x.shape
+        x = self.embeding(x)
+        x = self.position(x)
+        x = self.net(x)
+        x = torch.reshape(x, (batch, n, w, h))
+        return x
 
 
 def train(net, data_loader, loss_fn, optimizer, num_epochs, device):
@@ -255,7 +203,7 @@ if __name__ == "__main__":
         else "cpu"
     )
 
-    Net = TransUnet(1, 1).to(device)
+    Net = VIT(inchannel=1, out_channels=1).to(device)
 
     i = 0
     if os.path.exists("TransNet+.pth"):
